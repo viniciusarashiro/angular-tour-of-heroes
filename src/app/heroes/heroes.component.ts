8f61d21490eff4e2d27489fda094c448b88bcd11
vinicius <viniciusarashiro0@gmail.com>
2023-05-23 15:07:45 -0300

    feat: display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +8,11 @@ import { Hero } from '../hero'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id:1,
-    name: 'Windstorm'
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+
+  heroes = HEROES
+  protected readonly onselect = onselect;
 }

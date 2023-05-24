4c3d60cc83789665f9cec355d947f900d77b55e0
vinicius <viniciusarashiro0@gmail.com>
2023-05-24 17:53:24 -0300

    feat: add navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,5 +11,26 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location) {
+  }
+
+  getHero():void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero)
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
   @Input() hero?: Hero
+
+  ngOnInit(): void {
+    this.getHero()
+  }
+
+
 }
